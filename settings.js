e1e8c7e4ee758ad99c24d0d64430a7bd4f8f3c42
hacker ys <93207118+sl-code-herose-officially@users.noreply.github.com>
2022-07-26 20:55:58 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,1 +1,10 @@
+//═══════════════════════════════════════════════════════//
+//If you want to recode, reupload
+//or copy the codes/script,
+//pls give credit
+//no credit? i will take action immediately
+//© 2022 Xeon Bot Inc. Doge Bot 
+//Thank you to Lord Buddha, Family and Myself
+//════════════════════════════//
+
 
